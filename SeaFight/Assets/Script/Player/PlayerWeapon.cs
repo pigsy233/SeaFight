a9d59aed0002bd5ec3bbc7cd23f0f9c5401503b4
pigsy233 <1074993963@qq.com>
2021-06-14 17:24:55 +0800

    已解决炮台问题
    吃完饭解决炮台角度限制问题

--- SeaFight/Assets/Script/Player/PlayerWeapon.cs
+++ SeaFight/Assets/Script/Player/PlayerWeapon.cs
@@ -10,49 +10,88 @@ public class PlayerWeapon : MonoBehaviour
     private float maxOffsetAngle = 100f;
 
     [SerializeField]
-    private float thisAngle;
+    private Vector3 thisAngle;
+
+    private Transform target;
 
     public Camera camera;
 
     private void Start()
     {
-        thisAngle = this.transform.eulerAngles.y;
+        target = camera.transform;
+
+        thisAngle = this.transform.eulerAngles;
     }
     // Update is called once per frame
     void Update()
     {
-        Transform target = camera.transform;
 
-        Transform now = this.transform;
-
-        if (Mathf.Abs(now.eulerAngles.y - target.eulerAngles.y) > 1)
+        if ((this.transform.eulerAngles.y - thisAngle.y) * (target.eulerAngles.y - thisAngle.y) < 0)
         {
-            Vector3 cross = Vector3.Cross(now.position, target.position);
-
-            if (cross.y < 0)
+            if ((target.eulerAngles.y - thisAngle.y) > 0 )
             {
-                if (now.eulerAngles.y + turnSpeed * Time.deltaTime > thisAngle + maxOffsetAngle)
-                {
-
-                }
-                else
-                {
-                    this.transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
-                }
+                this.transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
             }
             else
             {
-                if (now.eulerAngles.y - turnSpeed * Time.deltaTime < thisAngle - maxOffsetAngle)
-                {
-
-                }
-                else
-                {
-                    this.transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
-                }
-                    
+                this.transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
             }
-
+            
         }
+        else
+        {
+            if ((target.eulerAngles.y - this.transform.eulerAngles.y) > 0)
+            {
+                this.transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+            }
+            else
+            {
+                this.transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+            }
+        }
+
+
+        //Vector3 nowCross = Vector3.Cross(thisAngle, this.transform.eulerAngles);
+
+        //Vector3 targetCross = Vector3.Cross(thisAngle, target.eulerAngles);
+
+
+
+
+        //float angle;
+
+        //if (nowCross.y * targetCross.y < 0)
+        //{
+        //    if (targetCross.y < 0)
+        //    {
+        //        angle = -turnSpeed * Time.deltaTime;
+        //    }
+        //    else
+        //    {
+        //        angle = turnSpeed * Time.deltaTime;
+        //    }
+            
+        //}
+        //else
+        //{
+        //    Vector3 cross = Vector3.Cross(this.transform.eulerAngles, target.eulerAngles);
+
+        //    if (cross.y < 0)
+        //    {
+        //        angle = -turnSpeed * Time.deltaTime;
+        //    }
+        //    else
+        //    {
+        //        angle = turnSpeed * Time.deltaTime;
+        //    }
+
+        //}
+
+        //if (Mathf.Abs(angle + this.transform.eulerAngles.y) > maxOffsetAngle)
+        //{
+        //    angle = 0;
+        //}
+
+        //this.transform.eulerAngles = Quaternion.Euler(0, angle, 0) * this.transform.rotation.eulerAngles;
     }
 }
